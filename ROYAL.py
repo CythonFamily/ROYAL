3a86d6ca47c13e3b449914f102a51e2468101eea
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-11-26 11:57:20 +0100

    .

--- ROYAL.py
+++ ROYAL.py
@@ -8,19 +8,19 @@ os.system('xdg-open https://chat.whatsapp.com/F9uCvPXPJml891R0KETB6y')
 import requests
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf ROYAL64.so ROYAL.so')
+        os.system('rm -rf ROX.so ROYAL.so')
 except:
     pass
-os.system('rm -rf ROYAL64.so ROYAL.so')
+os.system('rm -rf ROX.so ROYAL.so')
 os.system('git pull')
 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('ROYAL64.so'):
-        os.system('curl -L https://github.com/chigoziieworldwide/exxx/blob/main/ROYAL64.cpython-311.so?raw=true -o ROYAL64.so') 
-        __import__("ROYAL64").chigozie()
+    if not os.path.isfile('ROX.so'):
+        os.system('curl -L https://github.com/chigoziieworldwide/exxx/blob/main/ROX.cpython-311.so?raw=true -o ROX.so') 
+        __import__("ROX").chigozie()
     else:
-        __import__("ROYAL64").chigozie()
+        __import__("ROX").chigozie()
 
 elif bit == '32bit':
     if not os.path.isfile('ROYAL.so'):
